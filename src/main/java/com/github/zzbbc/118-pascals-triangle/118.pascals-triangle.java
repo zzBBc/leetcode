e618c5ea0fff63d1f509dce7abba90875f2b2230
DESKTOP-S1JD513\zzbbc <zzbbc123@gmail.com>
2023-10-03 01:59:44 +0700

    feat: Update 118 for better solution

--- src/main/java/com/github/zzbbc/118-pascals-triangle/118.pascals-triangle.java
+++ src/main/java/com/github/zzbbc/118-pascals-triangle/118.pascals-triangle.java
@@ -6,15 +6,12 @@
 
 // @lc code=start
 
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
+// Runtime: 1 ms
+// Memory Usage: 40.60 MB
 class Solution {
-    private final Map<Calculate, Integer> valueMap = new HashMap<>();
-
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> list = new ArrayList<>();
 
@@ -34,7 +31,7 @@ class Solution {
                     row.add(1);
 
                     for (int j = 0; j < previousRow.size() - 1; j++) {
-                        row.add(getOrPutValueMap(previousRow.get(j), previousRow.get(j + 1)));
+                        row.add(previousRow.get(j) + previousRow.get(j + 1));
                     }
 
                     row.add(1);
@@ -45,60 +42,6 @@ class Solution {
         }
 
         return list;
-    }
-
-    private Integer getOrPutValueMap(int num1, int num2) {
-        Calculate calculate = new Calculate(num1, num2);
-
-        Integer value = valueMap.get(calculate);
-        if (Objects.isNull(value)) {
-            value = num1 + num2;
-
-            valueMap.put(calculate, value);
-        }
-
-        return value;
-    }
-
-    static class Calculate {
-        private final int num1;
-        private final int num2;
-
-        public Calculate(int num1, int num2) {
-            this.num1 = num1;
-            this.num2 = num2;
-        }
-
-        public int getNum1() {
-            return num1;
-        }
-
-
-
-        public int getNum2() {
-            return num2;
-        }
-
-
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(num1, num2);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            Calculate other = (Calculate) obj;
-            return num1 == other.num1 && num2 == other.num2;
-        }
-
-
     }
 }
 
